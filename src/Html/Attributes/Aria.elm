04c0192e74cde7196f01ee78dcad2551684cd27f
Fabi <mail@fprein.de>
2016-07-07 16:31:53 +0200

    fix the aria-activedescendant attribute

--- src/Html/Attributes/Aria.elm
+++ src/Html/Attributes/Aria.elm
@@ -1,17 +1,18 @@
-module Html.Attributes.Aria exposing 
-    ( ariaActiveDescendant
-    , ariaChecked
-    , ariaDescribedby
-    , ariaDisabled
-    , ariaExpanded
-    , ariaHidden
-    , ariaLabel
-    , ariaLabelledby
-    , ariaReadonly
-    , ariaRequired
-    , ariaSelected
-    , role
-    )
+module Html.Attributes.Aria
+    exposing
+        ( ariaActiveDescendant
+        , ariaChecked
+        , ariaDescribedby
+        , ariaDisabled
+        , ariaExpanded
+        , ariaHidden
+        , ariaLabel
+        , ariaLabelledby
+        , ariaReadonly
+        , ariaRequired
+        , ariaSelected
+        , role
+        )
 
 {-| Additional attributes for html
 
@@ -33,28 +34,27 @@ module Html.Attributes.Aria exposing
 
 -}
 
-
 import Html exposing (Attribute)
 import Html.Attributes exposing (attribute)
 import Json.Encode as JE
 
 
 boolAttribute : String -> Bool -> Attribute msg
-boolAttribute name val = 
+boolAttribute name val =
     attribute name (JE.encode 0 <| JE.bool val)
 
 
 {-| Identifies the currently active descendant of a composite widget.
 See the [official specs](https://www.w3.org/TR/wai-aria/states_and_properties#aria-activedescendant).
 
-    div [ ariaActiveDescendant "id" ] [ text "Hello aria!" ]
+    div [ ariaActivedescendant "id" ] [ text "Hello aria!" ]
 -}
 ariaActiveDescendant : String -> Attribute msg
 ariaActiveDescendant =
-    attribute "aria-active-descendant"
+    attribute "aria-activedescendant"
 
 
-{-| Indicates the current "checked" state of checkboxes, radio buttons, and other widgets. 
+{-| Indicates the current "checked" state of checkboxes, radio buttons, and other widgets.
 See the [official specs](https://www.w3.org/TR/wai-aria/states_and_properties#aria-checked).
 
     div [ ariaChecked "true" ] [ text "Hello aria!" ]
@@ -62,102 +62,102 @@ See the [official specs](https://www.w3.org/TR/wai-aria/states_and_properties#ar
 ariaChecked : String -> Attribute msg
 ariaChecked =
     attribute "aria-checked"
- 
 
-{-| Identifies the element (or elements) that describes the object. 
+
+{-| Identifies the element (or elements) that describes the object.
 See the [official specs](https://www.w3.org/TR/wai-aria/states_and_properties#aria-describedby).
 
-    div [ ariaDescribedby "id" ] [ text "Hello aria!" ] 
+    div [ ariaDescribedby "id" ] [ text "Hello aria!" ]
 -}
 ariaDescribedby : String -> Attribute msg
 ariaDescribedby =
     attribute "aria-describedby"
- 
 
-{-| Indicates that the element is perceivable but disabled, so it is not editable or otherwise operable. 
+
+{-| Indicates that the element is perceivable but disabled, so it is not editable or otherwise operable.
 See the [official specs](https://www.w3.org/TR/wai-aria/states_and_properties#aria-disabled).
 
-    div [ ariaDisabled True ] [ text "Hello aria!" ] 
+    div [ ariaDisabled True ] [ text "Hello aria!" ]
 -}
 ariaDisabled : Bool -> Attribute msg
 ariaDisabled =
     boolAttribute "aria-disabled"
- 
 
-{-| Indicates whether the element, or another grouping element it controls, is currently expanded or collapsed. 
+
+{-| Indicates whether the element, or another grouping element it controls, is currently expanded or collapsed.
 See the [official specs](https://www.w3.org/TR/wai-aria/states_and_properties#aria-expanded).
 
-    div [ ariaExpanded "true" ] [ text "Hello aria!" ] 
+    div [ ariaExpanded "true" ] [ text "Hello aria!" ]
 -}
 ariaExpanded : String -> Attribute msg
 ariaExpanded =
     attribute "aria-expanded"
- 
 
-{-| Indicates that the element and all of its descendants are not visible or perceivable to any user as implemented by the author.  
+
+{-| Indicates that the element and all of its descendants are not visible or perceivable to any user as implemented by the author.
 See the [official specs](https://www.w3.org/TR/wai-aria/states_and_properties#aria-hidden).
 
-    div [ ariaHidden True ] [ text "Hello aria!" ] 
+    div [ ariaHidden True ] [ text "Hello aria!" ]
 -}
 ariaHidden : Bool -> Attribute msg
 ariaHidden =
     boolAttribute "aria-hidden"
 
 
-{-| Defines a string value that labels the current element. 
+{-| Defines a string value that labels the current element.
 See the [official specs](https://www.w3.org/TR/wai-aria/states_and_properties#aria-label).
 
-    div [ ariaLabel "label" ] [ text "Hello aria!" ] 
+    div [ ariaLabel "label" ] [ text "Hello aria!" ]
 -}
 ariaLabel : String -> Attribute msg
 ariaLabel =
     attribute "aria-label"
 
 
-{-| Identifies the element (or elements) that labels the current element. 
+{-| Identifies the element (or elements) that labels the current element.
 See the [official specs](https://www.w3.org/TR/wai-aria/states_and_properties#aria-labelledby).
 
-    div [ ariaLabelledby "id" ] [ text "Hello aria!" ] 
+    div [ ariaLabelledby "id" ] [ text "Hello aria!" ]
 -}
 ariaLabelledby : String -> Attribute msg
 ariaLabelledby =
     attribute "aria-labelledby"
 
 
-{-| Indicates that the element is not editable, but is otherwise operable. 
+{-| Indicates that the element is not editable, but is otherwise operable.
 See the [official specs](https://www.w3.org/TR/wai-aria/states_and_properties#aria-readonly).
 
-    div [ ariaReadonly True ] [ text "Hello aria!" ] 
+    div [ ariaReadonly True ] [ text "Hello aria!" ]
 -}
 ariaReadonly : Bool -> Attribute msg
 ariaReadonly =
     boolAttribute "aria-readonly"
 
 
-{-| Indicates that user input is required on the element before a form may be submitted. 
+{-| Indicates that user input is required on the element before a form may be submitted.
 See the [official specs](https://www.w3.org/TR/wai-aria/states_and_properties#aria-required).
 
-    div [ ariaRequired True ] [ text "Hello aria!" ] 
+    div [ ariaRequired True ] [ text "Hello aria!" ]
 -}
 ariaRequired : Bool -> Attribute msg
 ariaRequired =
     boolAttribute "aria-required"
 
 
-{-| Indicates the current "selected" state of various widgets. 
+{-| Indicates the current "selected" state of various widgets.
 See the [official specs](https://www.w3.org/TR/wai-aria/states_and_properties#aria-selected).
 
-    div [ ariaSelected "true" ] [ text "Hello aria!" ] 
+    div [ ariaSelected "true" ] [ text "Hello aria!" ]
 -}
 ariaSelected : String -> Attribute msg
 ariaSelected =
     attribute "aria-selected"
 
 
-{-| An attribute to support the role classification of elements. 
+{-| An attribute to support the role classification of elements.
 See the [official specs](https://www.w3.org/TR/role-attribute).
 
-    div [ role "button" ] [ text "Submit" ] 
+    div [ role "button" ] [ text "Submit" ]
 -}
 role : String -> Attribute msg
 role =
